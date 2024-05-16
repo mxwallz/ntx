8a21535304121ab96c56b8e3045249eea2b932de
Marcus Walls <marcuswalls.142@gmail.com>
2024-05-16 18:10:03 -0500

    change1 for update

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -6,6 +6,8 @@ import { LatestInvoice } from '@/app/lib/definitions';
 import { fetchLatestInvoices } from '@/app/lib/data';
 export default async function LatestInvoices() {
   const latestInvoices = await fetchLatestInvoices();
+
+  
   //console.log("invoices are: ", latestInvoices)
   return (
     <div className="flex w-full flex-col md:col-span-4">
